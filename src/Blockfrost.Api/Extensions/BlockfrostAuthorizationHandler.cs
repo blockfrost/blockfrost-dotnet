8afaec634a11d87654df192839207f319ea0c374
alex klee <alex.klee@outlook.com>
2021-08-08 15:43:24 +0200

    Add service extensions

---
+++ src/Blockfrost.Api/Extensions/BlockfrostAuthorizationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blockfrost.Api.Extensions
+{
+    public class BlockfrostAuthorizationHandler : DelegatingHandler
+    {
+        private readonly string _apiKey;
+
+        public BlockfrostAuthorizationHandler(string apiKey) : base()
+        {
+            _apiKey   = apiKey;
+        }
+        
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("project_id", _apiKey);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
