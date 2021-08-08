770c0999c569c6e1d16dc2f29d942e4e54a9928d
alex klee <alex.klee@outlook.com>
2021-08-08 15:42:03 +0200

    Add Options

---
+++ src/Blockfrost.Api/BlockfrostOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockfrost.Api
+{
+    public class BlockfrostOptions : Dictionary<string, BlockfrostProject>
+    {
+    }
+}
