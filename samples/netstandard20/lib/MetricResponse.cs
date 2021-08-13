57e58939491919e1fb2a5b14f0ab40d4b1145af7
Alexander Klee <aklee@pdc-online.com>
2021-08-13 13:40:42 +0200

    Add sample apps

---
+++ samples/netstandard20/lib/MetricResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Blockfrost.Api
+{
+    public partial class MetricResponse
+    {
+        /// <summary>Sum of all calls for a particular day</summary>
+        [Required(AllowEmptyStrings = true)]
+        [JsonPropertyName("calls")]
+        public int Calls { get; set; }
+
+        /// <summary>Starting time of the call count interval (ends midnight UTC) in UNIX time</summary>
+        [JsonPropertyName("time")]
+        public int Time { get; set; }
+    }
+}
