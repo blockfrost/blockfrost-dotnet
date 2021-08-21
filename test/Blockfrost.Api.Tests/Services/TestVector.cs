17eb91279ac376e5efab5f96f19c0b6213186c90
alex klee <alex.klee@outlook.com>
2021-08-21 15:24:17 +0200

    Add TestVector helper class

---
+++ test/Blockfrost.Api.Tests/Services/TestVector.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System;
+
+namespace Blockfrost.Api.Tests
+{
+    public class TestVector
+    {
+        private string _vectorId;
+
+        public TestVector(string vectorId1)
+        {
+            _vectorId = vectorId1;
+        }
+
+        internal static TestVector Load(string vectorId)
+        {
+            if (string.IsNullOrWhiteSpace(vectorId))
+            {
+                throw new System.ArgumentException($"'{nameof(vectorId)}' cannot be null or whitespace.", nameof(vectorId));
+            }
+
+            return new TestVector(vectorId);
+        }
+
+        public string GetFileText(string filename)
+        {
+            var projectDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory) // output directory
+                .Parent  // Debug / Release
+                .Parent  // bin
+                .Parent; // Blockfrost.Api.Tests
+            var vectorFile = new FileInfo(Path.Combine(projectDir.FullName, "dat", _vectorId, filename));
+            if (!vectorFile.Exists) throw new FileNotFoundException(vectorFile.FullName);
+            return File.ReadAllText(vectorFile.FullName);
+        }
+
+    }
+}
