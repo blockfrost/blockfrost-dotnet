350af88aaa23d76f6f38bd5e45a0965638da6dfc
alex klee <alex.klee@outlook.com>
2021-08-25 22:37:44 +0200

    Add Extensions project

---
+++ src/Blockfrost.Extensions/CardanoSharp/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CardanoSharp.Wallet;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Blockfrost.Extensions.CardanoSharp
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCardanoSharp(this IServiceCollection services)
+        {
+            return services.AddCardanoSharp("appsettings.json");
+        }
+        public static IServiceCollection AddCardanoSharp(this IServiceCollection services, string configPath)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .AddJsonFile(configPath);
+            
+            services.AddTransient<IWalletService, WalletService>();
+            return services;
+        }
+    }
+}
---
+++ src/Blockfrost.Extensions/SundaeSharp/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Blockfrost.Extensions
+{
+    public static class SundaeSwapServiceExtensions
+    {
+        public static IServiceCollection AddSundaeSharp(this IServiceCollection services)
+        {
+            // configure SundaeSwap services            
+
+            return services;
+        }
+    }
+}
