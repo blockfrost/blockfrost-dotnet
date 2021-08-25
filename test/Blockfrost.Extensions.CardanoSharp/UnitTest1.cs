96e4c83d77d84a671a557fb062019ebe4338e7ad
alex klee <alex.klee@outlook.com>
2021-08-26 01:05:43 +0200

    Make async

---
+++ test/Blockfrost.Extensions.CardanoSharp/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blockfrost.Extensions.CardanoSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
