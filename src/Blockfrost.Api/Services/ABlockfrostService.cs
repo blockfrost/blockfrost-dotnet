99e48c40ee01a5ad3189880de0b640df13b35c65
alex klee <alex.klee@outlook.com>
2021-08-27 10:17:11 +0200

    Saving files before refreshing line endings

--- src/Blockfrost.Api/Services/ABlockfrostService.cs
+++ src/Blockfrost.Api/Services/ABlockfrostService.cs
@@ -571,8 +571,8 @@ namespace Blockfrost.Api
         {
             public ObjectResponseResult(T responseObject, string responseText)
             {
-                this.Object = responseObject;
-                this.Text = responseText;
+                Object = responseObject;
+                Text = responseText;
             }
 
             public T Object { get; }
